f83d1cb0be15f9637424b1c7b37f1ccaa5412f9c
Detrital <Detritalw@outlook.com>
2024-11-24 15:46:58 +0800

    Add files via upload
    Signed-off-by: Detrital <Detritalw@outlook.com>

--- Class-Widget-CSV-import.cpp
+++ Class-Widget-CSV-import.cpp
@@ -1,10 +1,11 @@
-#define UNICODE
-#define _UNICODE
-
+#pragma comment(lib, "shlwapi.lib")
 #include <windows.h>
 #include <commdlg.h>
 #include <shlobj.h>
+#include <fcntl.h>
+#include <io.h>
 #include <bits/stdc++.h>
+#include <shlwapi.h>
 
 using namespace std;
 
@@ -15,16 +16,121 @@ HWND hEdit;
 // 声明回调函数
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-void scancsv(HWND hWnd){
-    MessageBoxW(hWnd, L"文件打开失败", L"请重试", MB_OK | MB_ICONSTOP);
+// 获取编辑框中的文本
+wstring GetEditText(HWND hEdit) {
+    int len = GetWindowTextLengthW(hEdit) + 1;
+    wchar_t* buffer = new wchar_t[len];
+    GetWindowTextW(hEdit, buffer, len);
+    wstring result(buffer);
+    delete[] buffer;
+    return result;
+}
+
+// 检查文件是否为 CSV 文件
+bool IsCsvFile(const wstring& filePath) {
+    return PathFileExistsW(filePath.c_str()) && !PathIsDirectoryW(filePath.c_str()) && 
+           _wcsicmp(PathFindExtensionW(filePath.c_str()), L".csv") == 0;
+}
+
+// 扫描 CSV 文件
+wstring scancsv(HWND hWnd, HWND hEdit) {
+    wstring filePath = GetEditText(hEdit);
+
+    if (filePath.empty()) {
+        MessageBoxW(hWnd, L"文本框为空", L"请输入文件路径", MB_OK | MB_ICONWARNING);
+        return L"";
+    }
+
+    if (!PathFileExistsW(filePath.c_str())) {
+        MessageBoxW(hWnd, L"文件不存在", L"请检查文件路径", MB_OK | MB_ICONSTOP);
+        return L"";
+    }
+
+    if (!IsCsvFile(filePath)) {
+        MessageBoxW(hWnd, L"文件不是.csv格式", L"请选择.csv文件", MB_OK | MB_ICONSTOP);
+        return L"";
+    }
+
+    return filePath;
+}
+
+void NumCsv2Json(HWND hWnd, HWND hEdit) {
+    wstring filePath = GetEditText(hEdit);
+    int timekc = 40;  // 保留这一行
+    string kindkc, text;
+    bool kc = false;  // 移动到这里
+    scancsv(hWnd, hEdit);
+
+    // 构建输出文件路径
+    wstring outputFilePath = filePath.substr(0, filePath.find_last_of(L"\\/") + 1) + L"课表.json";
+
+    // 重定向标准输入到 CSV 文件
+    FILE *inputFile = _wfreopen(filePath.c_str(), L"r", stdin);
+    if (inputFile == NULL) {
+        MessageBoxW(hWnd, L"无法打开输入文件", L"请检查文件路径", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    // 重定向标准输出到 JSON 文件
+    FILE *outputFile = _wfreopen(outputFilePath.c_str(), L"w", stdout);
+    if (outputFile == NULL) {
+        MessageBoxW(hWnd, L"无法创建输出文件", L"请检查文件路径", MB_OK | MB_ICONERROR);
+        fclose(inputFile);
+        return;
+    }
+
+    // 这里可以进行 CSV 到 JSON 的转换操作
+    // 例如：
+    int time = 0;
+    // int timekc = 0;  // 删除这一行
+    // bool kc = false;  // 移除这一行
+
+    while (true) {
+        // 读取时间
+        if (scanf("%d,", &time) != 1) {
+            printf("读取时间失败\n");
+            break;
+        }
+
+        if (time == 0) {
+            break;  // 如果读到的数字是 0，结束循环
+        }
+
+        // 读取时间kc
+        if (scanf("%d,", &timekc) != 1) {
+            printf("读取时间kc失败\n");
+            break;
+        }
+
+        // 读取两个字的文本
+        if (scanf("%2s,", &text) != 1) {
+            printf("读取文本失败\n");
+            break;
+        }
+
+        // 根据文本设置 kc 的值
+        if (strcmp(text.c_str(), "课程") == 0) kc = true;
+        else kc = false;
+
+        // 处理读取的数据
+        printf("time: %d, timekc: %d, kc: %s\n", time, timekc, kc ? "true" : "false");
+    }
+
+    // 关闭文件，恢复标准输入和输出
+    fclose(inputFile);
+    fclose(outputFile);
+    freopen("CONIN$", "r", stdin);  // 恢复标准输入到控制台
+    freopen("CONOUT$", "w", stdout);  // 恢复标准输出到控制台
+
+    return;
 }
 
 HFONT CreateSmoothFont(int height = 20, int weight = FW_NORMAL, const wchar_t* faceName = L"Segoe UI") {
-    LOGFONT lf = {0};
+    LOGFONTW lf = {0};  // 使用 LOGFONTW 而不是 LOGFONT
     lf.lfHeight = height;
     lf.lfWeight = weight;
     lf.lfQuality = CLEARTYPE_QUALITY;
-    wcsncpy_s(lf.lfFaceName, LF_FACESIZE, faceName, _TRUNCATE);
+    wcscpy_s(lf.lfFaceName, LF_FACESIZE, faceName);  // 使用 wcscpy_s
 
     HFONT hFont = CreateFontIndirectW(&lf);
 
@@ -59,7 +165,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     RegisterClassExW(&wcex);
     // 设置窗口的初始大小
-    hWnd = CreateWindowExW(0, L"SampleWindowClass", L"Updata", WS_OVERLAPPEDWINDOW,
+    hWnd = CreateWindowExW(0, L"SampleWindowClass", L"csv文件 转 ClassWidgets json 课表文件", WS_OVERLAPPEDWINDOW,
                            CW_USEDEFAULT, 0, 550, 300, NULL, NULL, hInstance, NULL);
 
     if (!hWnd) {
@@ -91,7 +197,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
                 SendMessage(hEdit, WM_SETFONT, (WPARAM)hFont, TRUE);
             }
 
-            CreateButton(hWnd, hInst, hFont, L"选择文件夹", 410, 10, 100, 25, 1);
+            CreateButton(hWnd, hInst, hFont, L"选择文件", 410, 10, 100, 25, 1);
             CreateButton(hWnd, hInst, hFont, L"确认并从.csv文件(第一列为单个时间段的时间长度)转换到.json课表文件", 10, 40, 500, 25, 2);
             CreateButton(hWnd, hInst, hFont, L"确认并从.csv文件(第一列为单个时间段的起始时间)转换到.json课表文件", 10, 70, 500, 25, 3);
             CreateButton(hWnd, hInst, hFont, L"确认并从.json课表文件转换到.csv文件", 10, 100, 500, 25, 4);
@@ -99,39 +205,40 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             CreateButton(hWnd, hInst, hFont, L"退出程序", 200, 130, 100, 25, 6);
             break;
         }
-        case WM_COMMAND:
+        case WM_COMMAND: {
             if (LOWORD(wParam) == 1) {
-                BROWSEINFOW bi = { 0 };
-                bi.hwndOwner = hWnd;
-                bi.pszDisplayName = new WCHAR[MAX_PATH];
-                bi.lpszTitle = L"选择一个文件夹";
-                LPITEMIDLIST pidl = SHBrowseForFolderW(&bi);
+                OPENFILENAMEW ofn;       // 使用宽字符版本的结构体
+                wchar_t szFile[260];    // 缓冲区用于存储文件名
+                ZeroMemory(&ofn, sizeof(ofn));
+                ofn.lStructSize = sizeof(ofn);
+                ofn.hwndOwner = hWnd;
+                ofn.lpstrFile = szFile; // 正确赋值
+                ofn.nMaxFile = sizeof(szFile) / sizeof(wchar_t);
+                ofn.lpstrFilter = L"CSV Files (*.csv)\0*.csv\0All Files (*.*)\0*.*\0";
+                ofn.nFilterIndex = 1;
+                ofn.lpstrFileTitle = NULL;
+                ofn.nMaxFileTitle = 0;
+                ofn.lpstrInitialDir = NULL;
+                ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
 
-                if (pidl != nullptr) {
-                    WCHAR path[MAX_PATH];
-                    if (SHGetPathFromIDListW(pidl, path)) {
-                        SetWindowTextW(hEdit, path);
-                    }
-                    CoTaskMemFree(pidl);
+                if (GetOpenFileNameW(&ofn)) { // 使用宽字符版本的函数
+                    SetWindowTextW(hEdit, ofn.lpstrFile);
                 }
-                delete[] bi.pszDisplayName;
             }
-            if (LOWORD(wParam) == 2){
-                scancsv(hWnd);
-                
+            // 其他命令处理
+            if (LOWORD(wParam) == 2) {
+                scancsv(hWnd, hEdit);
             }
-            if (LOWORD(wParam) == 3){
-                scancsv(hWnd);
-                
+            if (LOWORD(wParam) == 3) {
+                scancsv(hWnd, hEdit);
             }
-            if (LOWORD(wParam) == 4){
-                scancsv(hWnd);
-                
+            if (LOWORD(wParam) == 4) {
+                scancsv(hWnd, hEdit);
             }
             if (LOWORD(wParam) == 5) MessageBoxW(hWnd, L"其实我也不知道帮助是什么", L"谢谢", MB_OK | MB_ICONINFORMATION);
             if (LOWORD(wParam) == 6) exit(0);
             break;
-
+        }
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
