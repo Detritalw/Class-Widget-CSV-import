0f245b4ea6ca466c06ca7bd381355676223119e0
Detrital <Detritalw@outlook.com>
2025-01-01 12:22:03 +0800

    菜单更新
    Signed-off-by: Detrital <Detritalw@outlook.com>

--- README.md
+++ README.md
@@ -1,23 +1,17 @@
 # [Class Widgets](https://github.com/Class-Widgets/Class-Widgets) CSV import
 让我们用.csv表格编辑 [Class Widgets](https://github.com/Class-Widgets/Class-Widgets) 课表！将.csv文件转换为 Class Widgets Json 课表
 
-> [!TIP]
-> 本软件已加入插件广场，可前往 Class Widgets 插件广场 下载
-
-> [!IMPORTANT]
-> 本软件可作为软件直接使用，请下载`Class-Widget-CSV-import.exe`  
-> 若要作为插件使用，请在 Class Widgets 插件管理器中打开`"Class Widget Csv import"插件设置`
-
+## DEV
+若您想要看到最新版本，Github上的更新并不及时，请前往 [PCFS 存储站](http://123.129.241.101:30390/%E5%85%B1%E4%BA%AB/Class-Widget-CSV-import) 查看最新存储库和实时出版的测试版本
 
 ## 特点
-软件无依赖，单文件即可运行，支持作为插件使用
+软件无依赖，单文件即可运行
 
 ## 功能与计划
 - [x] 时间线转化
 - [x] 课程表转化
 - [x] 时间分区
 - [x] 支持中文
-- [x] 支持 Class Widgets 插件
 - [ ] 单双周课表
 - [ ] 每日时间线功能
 
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from loguru import logger
 from datetime import datetime
 from .ClassWidgets.base import PluginBase, SettingsBase, PluginConfig  # 导入CW的基类
 
-from PyQt5.QtWidgets import QHBoxLayout, QPushButton
+from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QFileDialog, QLineEdit
 from qfluentwidgets import ImageLabel
 import subprocess
 
@@ -20,7 +20,6 @@ class Plugin(PluginBase):  # 插件类
         self.cfg = PluginConfig(self.PATH, 'config.json')  # 实例化配置类
 
     def execute(self):  # 自启动执行部分
-
         logger.success('Plugin1 executed!')
         logger.info(f'Config path: {self.PATH}')
 
@@ -29,7 +28,7 @@ class Plugin(PluginBase):  # 插件类
         self.cfg.update_config()  # 更新配置
 
         if self.method.is_get_notification():
-            logger.warning('warning', f'Plugin1 got notification! Title: {self.cw_contexts["Notification"]["title"]}')
+            logger.warning(f'Plugin1 got notification! Title: {self.cw_contexts["Notification"]["title"]}')
 
             if self.cw_contexts['Notification']['state'] == 0:  # 如果下课
                 self.method.subprocess_exec(self.cfg['name'], self.cfg['action'])  # 调用CW方法构建自动化
@@ -39,12 +38,40 @@ class Plugin(PluginBase):  # 插件类
 class Settings(SettingsBase):
     def __init__(self, plugin_path, parent=None):
         super().__init__(plugin_path, parent)
+        self.PATH = plugin_path  # 确保 plugin_path 被正确设置
         uic.loadUi(f'{self.PATH}/settings.ui', self)  # 加载设置界面
 
         # 添加打开转换器按钮
         self.openButton = self.findChild(QPushButton, 'openButton')
-        self.openButton.clicked.connect(self.open_converter)
+        if self.openButton:
+            self.openButton.clicked.connect(self.open_converter)
+        else:
+            logger.error('openButton not found in settings.ui')
+
+        self.helpButton = self.findChild(QPushButton, 'helpButton')
+        if self.helpButton:
+            self.helpButton.clicked.connect(self.open_help)
+        else:
+            logger.error('helpButton not found in settings.ui')
 
     def open_converter(self):
         subprocess.Popen([f'{self.PATH}/cw-CSV-import.exe'])
         logger.info('打开转换器')
+
+    def open_help(self):
+        subprocess.Popen([f'{self.PATH}/help.exe'])
+        logger.info('需要帮助')
+
+# 菜单类
+class Menu:
+    def __init__(self):
+        # 初始化 Settings 类，并传递正确的 plugin_path 参数
+        self.settings = Settings(plugin_path='/d:/Work/1.1.7-b4/plugins/Class-Widget-CSV-import', parent=None)
+
+    def show_settings(self):
+        self.settings.show()
+
+# 初始化并显示菜单
+if __name__ == "__main__":
+    menu = Menu()
+    menu.show_settings()
